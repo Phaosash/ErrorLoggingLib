1277e6caa83f8c95f911ff856cbfdab0601c997d
Michael McKIE <michael_mckie@hotmail.com>
2025-09-20 11:23:27 +0800

    Added log rotation and clean up

--- ErrorLoggingLibrary/LoggingManager.cs
+++ ErrorLoggingLibrary/LoggingManager.cs
@@ -2,29 +2,29 @@
 
 namespace ErrorLoggingLibrary;
 
-//  How to use: string logFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "logs", "app_log.txt");
-public sealed class LoggingManager {
-    private static readonly Lazy<LoggingManager> _instance = new(() => new LoggingManager("default_log.txt"));
-    private readonly FileLogger _fileLogger;
 
-    private LoggingManager (string filePath){
-        _fileLogger = new FileLogger(filePath);
+public sealed class LoggingManager {
+    private const int DefaultRetentionPeriod = 7;
+    private static readonly Lazy<LoggingManager> _instance = new(() => new LoggingManager(DefaultRetentionPeriod));
+    private readonly FileManager _fileManager;
+    private readonly int _fileRetentionPeriod;
+
+    private LoggingManager (int fileRetentionPeriod){
+        _fileRetentionPeriod = fileRetentionPeriod < 0 ? DefaultRetentionPeriod : fileRetentionPeriod;
+        _fileManager = new FileManager(_fileRetentionPeriod);
     }
 
     public static LoggingManager Instance => _instance.Value;
 
-    //  This method logs an informational message using an internal file logger, formatting the log entry to include the provided message.
-    public async Task LogInformation (string message){
-        await _fileLogger.LogAsync(LogLevel.Information, message, null, (state, exception) => $"Message: {state}");
+    public void LogInformation (string message){
+        _fileManager.Log(LogLevel.Information, message, null, (state, exception) => $"Message: {state}");
     }
 
-    //  This method logs an error message along with exception details using an internal file logger, including the exception message and stack trace for better diagnostics.
-    public async Task LogError (Exception ex, string message){
-        await _fileLogger.LogAsync(LogLevel.Error, message, ex, (state, exception) => $"{state}: {exception?.Message}\nStackTrace: {exception?.StackTrace}");
+    public void LogError (Exception ex, string message){
+        _fileManager.Log(LogLevel.Error, message, ex, (state, exception) => $"{state}: {exception?.Message}\nStackTrace: {exception?.StackTrace}");
     }
 
-    //  This method logs a warning message using an internal file logger, prefixing the log entry with "Warning:" to highlight its severity.
-    public async Task LogWarning (string message){
-        await _fileLogger.LogAsync(LogLevel.Warning, message, null, (state, exception) => $"Warning: {state}");
+    public void LogWarningWithException (Exception ex, string message){
+        _fileManager.Log(LogLevel.Warning, message, ex, (state, exception) => $"Warning: {state}. Exception: {exception?.Message}");
     }
 }
